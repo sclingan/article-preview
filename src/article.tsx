5361f0c47289bc12e2b2dfab57005c9447597844
Scott Clingan <sclingan1@gmail.com>
2023-04-11 14:25:09 -0500

    Testing inline svg

--- src/article.tsx
+++ src/article.tsx
@@ -1,13 +1,16 @@
 export default function showSocial() {
     const social = document.getElementById('social');
     const profile = document.getElementById('profile');
+    
 
+    // If profile is shown, slide in Icons
     if(profile?.className === 'profile-container') {
         profile?.classList.remove('profile-container');
         profile?.classList.add('sr-only');
         social?.classList.remove('sr-only');
         social?.classList.add('social-container');
     } else {
+        // Hide Icons, show profile
         profile?.classList.remove('sr-only');
         profile?.classList.add('profile-container');
         social?.classList.remove('social-container')
